59a61eed9af39633489a9509510223b21e40268a
vlvlearning <vlvlearing@gmail.com>
2020-08-08 23:18:12 +0530

    Day2 English and few updates on Angular

--- AngProj1/src/app/app.module.ts
+++ AngProj1/src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';  
+import { AppRoutingModule } from './app-routing.module';  
+import { AppComponent } from './app.component';  
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
+import { HttpClientModule } from '@angular/common/http';  
+import {DataTablesModule} from 'angular-datatables';  
+import { StudentListComponent } from './student-list/student-list.component';  
+import { AddStudentComponent } from './add-student/add-student.component';  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    StudentListComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,  
+    AppRoutingModule,  
+    FormsModule,  
+    ReactiveFormsModule,  
+    HttpClientModule,  
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
